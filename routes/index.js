0ca0b085c607f21f55b1da3ec0fb228ef0ae7913
Binod Chaudhary <thebinod7@gmail.com>
2017-08-12 23:43:15 +0545

    Add property and list on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Property = require('../models/property');
 
-router.get('/',function(req,res){
-  const data = {
-    title : 'Nepal Agents'
-  }
-  res.render('index',data);
+router.get('/', (req, res) => {
+  Property
+    .find({
+      $and : [
+        { currentStatus : 'publish' },
+        { isActive : true }
+      ]
+    })
+    .sort('-dateAdded')
+    .limit(8)
+    .exec(function (err, docs) {
+      if(err){
+        res.json({success : false, msg : 'Failed to list!'});
+      } else {
+        var data = {
+          property: docs,
+          title : 'Home - Nepal Agents'
+        };
+        console.log(data);
+        res.render('index',data);
+      }
+    });
 });
 
 module.exports = router;
--- routes/property.js
+++ routes/property.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Users= require('../models/users');
+const Property= require('../models/property');
 const nodemailer = require('nodemailer');
 const session = require('express-session');
 const jwt = require('jsonwebtoken');
@@ -12,11 +13,78 @@ const dashboardLayoutData = {
   layout: 'layouts/dashboard'
 };
 
-router.get('/add',function(req,res){
+var storage = multer.diskStorage({
+  destination: './public/assets/img/uploads/',
+  filename (req, file, cb) {
+    cb(null,
+      file.fieldname + '-' + Date.now() + '.' +
+      file.originalname.split('.')[file.originalname.split('.').length - 1]
+    );
+  }
+});
+
+const upload = multer({ storage });
+
+router.post('/uploadImages', upload.single('file'), function(req, res) {
+  res.json({
+    error: false,
+    result: req.file.filename
+  });
+});
+
+router.get('/add', auth, function(req,res){
   const data = Object.assign(dashboardLayoutData, {
         title:  'Property - Add'
       });
     res.render('property/add', data);
+});
+
+router.post('/save', (req, res) => {
+  const images = req.body.images.split(',');
+  const amenities = req.body.amenities.split(',');
+
+  const featuredImgUrl = images[0];
+  images.splice(0, 1);
+
+  const payload = Object.assign({}, req.body, {
+    featuredImgUrl,
+    images,
+    amenities,
+    userId : req.session.userId,
+  });
+  const property = new Property(payload);
+  property.save((err) => {
+    if(err) {
+      console.log(err);
+      req.flash('error', 'ERROR! Failed to add property listing');
+      res.redirect('/property/add');
+    } else {
+      req.flash('success', 'SUCCESS! Property listing added successfully!');
+      res.redirect('/property/add');
+    }
+  });
+});
+
+router.get('/list', (req, res) => {
+  Property
+    .find()
+    .exec(function (err, docs) {
+      if(err){
+        res.json({success : false, msg : 'Failed to list!'});
+      } else {
+        res.json({success:true,msg:'Success',result:docs});
+      }
+    });
+});
+
+router.delete('/delete/:id',function (req,res) {
+  Property.remove({ _id: req.params.id }, function(err,docs) {
+    if (err) throw err;
+
+    res.json({success : true, msg :'Success', result : docs});
+    // req.flash('success', 'SUCCESS! Property deleted successfully!');
+    // res.redirect('/estate/my-listings');
+  });
 });
 
 module.exports = router;
